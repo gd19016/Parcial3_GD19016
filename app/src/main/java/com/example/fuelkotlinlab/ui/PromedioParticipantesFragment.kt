367e7dc7bd2fc574b0cb5b0bd69f412f41bb9b37
Roberto Gutierrez <roberto.gutierrez@mitratech.com>
2022-06-13 22:13:54 -0600

    Promedio de participantes

--- app/src/main/java/com/example/fuelkotlinlab/ui/PromedioParticipantesFragment.kt
+++ app/src/main/java/com/example/fuelkotlinlab/ui/PromedioParticipantesFragment.kt
@@ -5,56 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.fuelkotlinlab.R
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.json.responseJson
+import com.github.kittinunf.result.Result
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PromedioParticipantesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PromedioParticipantesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_promedio_participantes, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PromedioParticipantesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PromedioParticipantesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val fragment = inflater.inflate(R.layout.fragment_promedio_participantes,
+            container, false)
+        val btn: Button = fragment.findViewById(R.id.obtenerButton)
+        val editText: EditText = fragment.findViewById(R.id.idJuegoEditText)
+        val resultText: TextView = fragment.findViewById(R.id.promedioText)
+        btn.setOnClickListener {
+            Fuel.get(
+                "/parcial3/ws_promedio_participantes.php",
+                listOf("juego" to editText.text)
+            ).responseJson { _, _, result ->
+                when (result) {
+                    is Result.Failure -> {
+                        resultText.text = result.getException().toString()
+                    }
+                    is Result.Success -> {
+                        val data = result.get().array()
+                        if (data.length() > 0) {
+                            resultText.text =
+                                data.getJSONObject(0).getString("PROMEDIO")
+                        } else {
+                            resultText.text = getString(R.string.no_juego)
+                        }}}}
+        }
+        return fragment
     }
 }
